45f4c7486d2a477724dd9f223007e57e79db4780
JJgotBored <jasonwbere@gmail.com>
2021-01-08 00:52:38 -0500

    - OneTimePadAPI.c 	- completed buildOTPKey function 	- completed freeOTPKey function - OneTimePadAPI.h 	- added to OTPErrorCode

--- include/OneTimePadAPI.h
+++ include/OneTimePadAPI.h
@@ -3,8 +3,9 @@
 
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
 
-typedef enum OTPErrorCode{ok, invalidName }OTPErrorCode;
+typedef enum OTPErrorCode{ok, nullName, nullKeyPointer }OTPErrorCode;
 
 typedef struct OTPKey{
     
@@ -16,6 +17,6 @@ typedef struct OTPKey{
 
 OTPErrorCode buildOTPKey(char* name, unsigned int seed, OTPKey** keyPointer );
 
-int freeOTPKey(OTPKey** keyPointer);
+OTPErrorCode freeOTPKey(OTPKey* keyPointer);
 
 #endif
--- src/OneTimePadAPI.c
+++ src/OneTimePadAPI.c
@@ -1,23 +1,45 @@
 #include<stdio.h>
 #include"OneTimePadAPI.h"
 
-
+/** Function to create one time pad encryption key
+ * @pre name must not be null
+        seed must be unsigned int
+        keyPointer must not point to anything
+ * @post keyPointer will point to new OTPKey
+ * @return returns OTPErrorCode indecating success or failure
+**/
 OTPErrorCode buildOTPKey(char* name, unsigned int seed, OTPKey** keyPointer ){
     if(name == NULL){
         return 1;
     }
-
     OTPKey* newKey = (OTPKey*)(malloc(sizeof(OTPKey)*1));
-
+    
+    //set name
     newKey->name = (char*)(malloc(sizeof(char)*(strlen(name)+1))); 
     strcpy(newKey->name, name);
-
+    //set seed
     newKey->seed = seed;
-
+    //set offset
     srand(seed);
     for(int i = 0; i < 280; i++){
         newKey->offset[i] = rand()%67;
     }
+
+    *keyPointer = newKey;
+
+    return 0;
+}
+/** Function to free one time pad encryption key
+ * @pre keyPointer must not be null
+ * @post keyPointer is deallocated 
+ * @return returns OTPErrorCode indecating success or failure
+**/
+OTPErrorCode freeOTPKey(OTPKey* keyPointer){
+    if(keyPointer == NULL){
+        return 2;
+    }
+    free(keyPointer->name);
+    free(keyPointer);
 
     return 0;
 }
